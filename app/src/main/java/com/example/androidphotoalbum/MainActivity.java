ab5094dbe995e76f82cd3b670cc97ea2034737f3
Farkhad Rakhimzhanov <farkhad@gmail.com>
2019-08-16 21:19:27 +0600

    Modified layout as in task, implemented logic for switching between activities

--- app/src/main/java/com/example/androidphotoalbum/MainActivity.java
+++ app/src/main/java/com/example/androidphotoalbum/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.androidphotoalbum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        if (textView.getText().equals("")) {
+            int randomNum = new Random().nextInt(100);
+            textView.setText("http://myfile.org/" + randomNum);
+        }
+
+        Button backBtn = findViewById(R.id.back);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        Button nextBtn = findViewById(R.id.next);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
